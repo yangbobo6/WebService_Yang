f977f88a2a1221ecac43eb3e4db7cda2c697b41e
yyangbobo <1360664142@qq.com>
2022-03-20 21:36:56 +0800

    web开始

---
+++ WebServiceNio/core/src/main/java/com/yangbo/webserver/core/network/dispatcher/AbstractDispatcher.java
@@ -0,0 +1,10 @@
+package com.yangbo.webserver.core.network.dispatcher;
+
+/**
+ * @Author: yangbo
+ * @Date: 2022-03-19-22:16
+ * @Description:
+ */
+public abstract class AbstractDispatcher {
+
+}
--- WebServiceNio/core/src/main/java/com/yangbo/webserver/core/network/endpoint/NioEndpoint.java
+++ WebServiceNio/core/src/main/java/com/yangbo/webserver/core/network/endpoint/NioEndpoint.java
@@ -1,11 +1,16 @@
 package com.yangbo.webserver.core.network.endpoint;
 
+import java.nio.channels.ServerSocketChannel;
+
 /**
  * @Author: yangbo
  * @Date: 2022-03-19-17:03
- * @Description:
+ * @Description:  NIO
  */
 public class NioEndpoint extends Endpoint{
+    private ServerSocketChannel server;
+
+
 
     @Override
     public void start(int port) {
